41169a8e7075c5da4b0b88718d463727b6421965
Asad Ikhlas <asadikhlas03@gmail.com>
2019-03-23 23:08:26 +0500

    commit

---
+++ Template_string/template_string_exercise_1.js
@@ -0,0 +1,7 @@
+const name = 'asad';
+const lastname = 'ikhlas';
+let age = 22;
+let profession = 'developer';
+let salary = 25000;
+
+console.log(`My name is ${name} ${lastname}, I'm ${age}, I work as ${profession} and make $${salary}`)
