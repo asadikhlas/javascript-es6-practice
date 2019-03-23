5636b31e8ef7fab2c1d72ddbc8f81f4fe4274000
Asad Ikhlas <asadikhlas03@gmail.com>
2019-03-23 19:08:35 +0500

    COMMIT

---
+++ Template_string/template_string.js
@@ -0,0 +1,9 @@
+function returnemploye(){
+    const name = "Asad";
+    const age = 22;
+    const position = "manager";
+
+    console.log(`My name is ${name} my age is ${age} my position is ${position}`)
+}
+
+returnemploye();
