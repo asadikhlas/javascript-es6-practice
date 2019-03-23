e094d9a4b24c0db9b32bce90bdc5db51df38212e
Asad Ikhlas <asadikhlas03@gmail.com>
2019-03-23 23:40:58 +0500

    commit

---
+++ ForEach_Loop/forEach.js
@@ -0,0 +1,9 @@
+const names = ['asad','umair','shahbaz','ali']
+
+// for( var i = 0; i < names.length; i++ ){
+// console.log(names[i])
+// }
+
+names.forEach(function(name){
+    console.log(name)
+})
