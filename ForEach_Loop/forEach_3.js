32990e382020d6d8666c4958aa3f75515e11fa1a
Asad Ikhlas <asadikhlas03@gmail.com>
2019-03-23 23:55:17 +0500

    commit

---
+++ ForEach_Loop/forEach_3.js
@@ -0,0 +1,13 @@
+const purchases = [
+    {quantity:2,amount:100},
+    {quantity:5,amount:100},
+    {quantity:10,amount:100}
+]
+
+var total = 0;
+
+purchases.forEach(function(item){
+    total += item.quantity * item.amount
+})
+
+console.log(total)
