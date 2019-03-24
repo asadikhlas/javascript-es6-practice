b38b68ae1d21ef4988671d4f133d9bd9cf159bbe
Asad Ikhlas <asadikhlas03@gmail.com>
2019-03-24 15:49:12 +0500

    commit

---
+++ Map_helper/map_helper_1.js
@@ -0,0 +1,13 @@
+const numbers = [1, 2, 3, 4, 5];
+const otherArray = numbers.map(function(number){
+    return number * 10
+})
+
+
+// var otherArray = [];
+
+// for (var i = 0; i < number.length; i++) {
+//   otherArray.push(number[i] * 10);
+// }
+
+console.log(otherArray);
