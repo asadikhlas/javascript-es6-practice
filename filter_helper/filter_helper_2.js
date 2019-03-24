5bc9f9fe19d351e89fefb81429eaa8a46d124764
Asad Ikhlas <asadikhlas03@gmail.com>
2019-03-24 16:48:25 +0500

    commit

---
+++ filter_helper/filter_helper_2.js
@@ -0,0 +1,7 @@
+const products = [
+    { name: "Iphone", price: 200 },
+    { name: "Motorola", price: 70 },
+    { name: "Samsung", price: 150 },
+    { name: "Sony", price: 98 },
+    { name: "Windows pone", price: 150 }
+  ];
