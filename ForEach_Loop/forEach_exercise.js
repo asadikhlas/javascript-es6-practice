5e44fccfea7ade9531c368a87b7802f4d59522fc
Asad Ikhlas <asadikhlas03@gmail.com>
2019-03-24 14:54:56 +0500

    commit

--- ForEach_Loop/forEach_exercise.js
+++ ForEach_Loop/forEach_exercise.js
@@ -1,7 +1,30 @@
-var products = [
+const products = [
     {name:'Iphone',price:200},
     {name:'Motorola',price:70},
     {name:'Samsung',price:150},
     {name:'Sony',price:98},
     {name:'Windows pone',price:10}
  ]; 
+
+ let template = '';
+ products.forEach(function(product){
+   
+    function discount() {
+        if(product.price < 100){
+            return `
+            <span>On sale </span>
+            `
+        }
+    }
+
+     template += `
+     <div class="product">
+        <h1>${product.name}</h1>
+        ${discount()}
+        <strong>Price: $ ${product.price} </strong>
+        
+     </div>
+     `
+ })
+
+ document.body.insertAdjacentHTML("afterbegin",template)
