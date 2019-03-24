5f0a47428d1d21b2c7c907128be67c32bfed586f
Asad Ikhlas <asadikhlas03@gmail.com>
2019-03-24 16:46:32 +0500

    commit

--- filter_helper/filter-helper_1.js
+++ filter_helper/filter-helper_1.js
@@ -3,7 +3,13 @@ const products = [
     { name: "Motorola", price: 70 },
     { name: "Samsung", price: 150 },
     { name: "Sony", price: 98 },
-    { name: "Windows pone", price: 10 }
+    { name: "Windows pone", price: 150 }
   ];
 
-  
+
+
+const result = products.filter(function(product){
+    return product.price === 150
+})
+
+console.log(result)
