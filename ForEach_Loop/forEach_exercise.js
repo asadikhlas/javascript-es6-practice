9dc9bf6f4525cdb57f5c6f5ba13fb94d73b1df2c
Asad Ikhlas <asadikhlas03@gmail.com>
2019-03-24 14:55:45 +0500

    commit

--- ForEach_Loop/forEach_exercise.js
+++ ForEach_Loop/forEach_exercise.js
@@ -1,30 +1,29 @@
 const products = [
-    {name:'Iphone',price:200},
-    {name:'Motorola',price:70},
-    {name:'Samsung',price:150},
-    {name:'Sony',price:98},
-    {name:'Windows pone',price:10}
- ]; 
+  { name: "Iphone", price: 200 },
+  { name: "Motorola", price: 70 },
+  { name: "Samsung", price: 150 },
+  { name: "Sony", price: 98 },
+  { name: "Windows pone", price: 10 }
+];
 
- let template = '';
- products.forEach(function(product){
-   
-    function discount() {
-        if(product.price < 100){
-            return `
+let template = "";
+products.forEach(function(product) {
+  function discount() {
+    if (product.price < 100) {
+      return `
             <span>On sale </span>
-            `
-        }
+            `;
     }
+  }
 
-     template += `
+  template += `
      <div class="product">
         <h1>${product.name}</h1>
         ${discount()}
         <strong>Price: $ ${product.price} </strong>
         
      </div>
-     `
- })
+     `;
+});
 
- document.body.insertAdjacentHTML("afterbegin",template)
+document.body.insertAdjacentHTML("afterbegin", template);
