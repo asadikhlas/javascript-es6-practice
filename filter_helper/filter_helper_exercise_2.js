e849f3cacda8e47e69f102590055df4b81aec8ae
Asad Ikhlas <asadikhlas03@gmail.com>
2019-03-25 20:26:46 +0500

    comit

--- filter_helper/filter_helper_exercise_2.js
+++ filter_helper/filter_helper_exercise_2.js
@@ -1,28 +1,28 @@
 const channels = [
-   {name:'HBO',premium:true},
-   {name:'LIFE',premium:false},
-   {name:'Max',premium:true},
-   {name:'Cooking channel',premium:false},
-   {name:'WOBI',premium:false}
+  { name: "HBO", premium: true },
+  { name: "LIFE", premium: false },
+  { name: "Max", premium: true },
+  { name: "Cooking channel", premium: false },
+  { name: "WOBI", premium: false }
 ];
 const user = {
-   name:'Francis',
-   premium:true,
-   premiumChannels:function(){
-       // GET THE PREMIUM CHANNELS IS "PREMIUM" IS true
-       const $this = this
-       return channels.filter(function(channel){
-           return channel.premium && $this.premium
-       })
-   },
-   channels:function(){
-       // GET THE NON-PREMIUM CHANNELS
-       return channels.filter(function(channel){
-           return channel.premium === false;
-       })
-   }
-}
-console.log(user.premiumChannels())
+  name: "Francis",
+  premium: true,
+  premiumChannels: function() {
+    // GET THE PREMIUM CHANNELS IS "PREMIUM" IS true
+    const $this = this;
+    return channels.filter(function(channel) {
+      return channel.premium && $this.premium;
+    });
+  },
+  channels: function() {
+    // GET THE NON-PREMIUM CHANNELS
+    return channels.filter(function(channel) {
+      return channel.premium === false;
+    });
+  }
+};
+console.log(user.premiumChannels());
 // brings HBO and MAX
-console.log(user.channels())
+console.log(user.channels());
 // brings LIFE, COOCKING CHANNEL AND WOBI
