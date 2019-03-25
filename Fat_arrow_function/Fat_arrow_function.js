890ca4b5cf9f4e1dc38daf33e13abb5edff7226a
Asad Ikhlas <asadikhlas03@gmail.com>
2019-03-25 22:41:41 +0500

    commit

---
+++ Fat_arrow_function/Fat_arrow_function.js
@@ -0,0 +1,21 @@
+
+//OLD ES6 CODING
+
+// const value = function(name, age){
+//     return `My name is ${name} and age is ${age}`
+// }
+
+// console.log(value("asad",22))
+
+//ES6 CODING
+
+// const value = (name, age) => {
+//     return `My name is ${name} and age is ${age}`
+// }
+
+// console.log(value("asad",22))
+
+const value = (x, y) => x + y;
+console.log(value(2,5))
+
+
