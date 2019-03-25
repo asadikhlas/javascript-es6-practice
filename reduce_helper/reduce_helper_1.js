28e65d7383df40cb317023ed15c391f51eec9ca5
Asad Ikhlas <asadikhlas03@gmail.com>
2019-03-25 21:51:54 +0500

    commit

---
+++ reduce_helper/reduce_helper_1.js
@@ -0,0 +1,12 @@
+const prevoius = 200;
+const current = [
+    {expense:'Guitar',amount:200},
+    {expense:'Miic',amount:100},
+    {expense:'starp',amount:100}
+]
+
+const result = current.reduce(function(sum,expense){
+    return sum + expense.amount
+},prevoius)
+
+console.log(result)
