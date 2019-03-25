37f24a47849d07901d841e2d7e81e711c2496b37
Asad Ikhlas <asadikhlas03@gmail.com>
2019-03-25 20:14:48 +0500

    xommit

---
+++ filter_helper/app.js
@@ -0,0 +1,58 @@
+const initialData = {
+  React: {
+    title: "React",
+    questions: [
+      {
+        question: "What is React?",
+        answer: "A library for managing user interfaces"
+      },
+      {
+        question: "Where do you make Ajax requests in React?",
+        answer: "The componentDidMount lifecycle event"
+      }
+    ]
+  },
+  JavaScript: {
+    title: "JavaScript",
+    questions: [
+      {
+        question: "What is a closure?",
+        answer:
+          "The combination of a function and the lexical environment within which that function was declared."
+      }
+    ]
+  }
+};
+
+
+// const a = Object.keys(initialData).map(item => {
+//     const {title, questions} =
+
+// })
+
+
+// const data = {
+//     react: {
+//         javascript: 'acsa',
+//         skills: {
+
+//         },
+
+//     Javascript: {
+//         skills: ['abc', 'sdada'],
+//         id: 1
+//     },
+
+//     react: {
+//         javascript: 'sdasda'
+//     }
+
+//     }
+// }
+
+
+// console.log(data['react'].javascript)
+
+// const a = Object.assign([], data)
+
+// console.log(a)
