c7817aeda25e6a09e4be242746ffc74224440eba
Asad Ikhlas <asadikhlas03@gmail.com>
2019-03-25 21:17:45 +0500

    commit

--- Every_Some_helper/every_some_helper_1.js
+++ Every_Some_helper/every_some_helper_1.js
@@ -1,5 +1,25 @@
 const students = [
-    {name:'asad',grade:7},
-    {name:'umair',grade:8},
-    {name:'shahbaz',grade:5}
+    {name:'asad',grade:6},
+    {name:'umair',grade:7},
+    {name:'shahbaz',grade:7}
 ]
+
+
+let allStudentsPassedTheCourse = students.every(function(student){
+    return student.grade >= 6 
+})
+
+// let allStudentsPassedTheCourse = false;
+
+// for(let i = 0; i < students.length; i++){
+//     let student = students[i]
+//     if(student.grade < 6){
+//         allStudentsPassedTheCourse = false;
+//         break;
+//     }else{
+
+//         allStudentsPassedTheCourse = true
+//     }
+// }
+
+console.log(allStudentsPassedTheCourse)
