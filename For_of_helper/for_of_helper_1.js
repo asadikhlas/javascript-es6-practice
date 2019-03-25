0d514c73ce8fc1cca496a6b965ad9f5c92008ba0
Asad Ikhlas <asadikhlas03@gmail.com>
2019-03-25 22:30:12 +0500

    commit

---
+++ For_of_helper/for_of_helper_1.js
@@ -0,0 +1,14 @@
+// const names = ['asad','umair','shahbaz']
+
+// for(let name of names){
+//     console.log(name)
+// }
+
+let numbers = [10, 20, 30, 40, 50];
+let total = 0;
+
+for (number of numbers) {
+  total += number;
+}
+
+console.log(total);
