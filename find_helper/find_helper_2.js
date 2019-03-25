ce848acc6c1c796a7a981f7db1d84e2d05ef866d
Asad Ikhlas <asadikhlas03@gmail.com>
2019-03-25 20:44:06 +0500

    commmit

--- find_helper/find_helper_2.js
+++ find_helper/find_helper_2.js
@@ -1,1 +1,6 @@
-const 
+const cars = [
+  { brand: "Ford", price: 500, available: 2, type: "Sport car" },
+  { brand: "Nissan", price: 200, available: 5, type: "Wagon" },
+  { brand: "Chavy", price: 300, available: 4, type: "Sport car" },
+  { brand: "Porsche", price: 100, available: 1, type: "Urban" }
+];
