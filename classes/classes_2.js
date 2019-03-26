4a5a6cd7a34f8efff1c4cbd18b38d1dd914b7322
Asad Ikhlas <asadikhlas03@gmail.com>
2019-03-26 23:00:44 +0500

    comit

--- classes/classes_2.js
+++ classes/classes_2.js
@@ -1,15 +1,37 @@
+// class Car {
+//     constructor(){
+//         this.status = options.status;
+//         this.wheels = options.wheels;
+//         this.avail = options.avail;
+//     }
+// }
+
+// const car = new Car({
+//     status:'New',
+//     wheels:4,
+//     avail:()=>{console.log('avail')}
+// })
 class Car {
-    constructor(options){
-        this.status = options.status;
-        this.wheels = options.wheels;
-        this.avail = options.avail;
+    constructor(){
+        this.condition = 'NEW';
+        this.wheels = '4';
+        this.avail = () => {
+            console.log('avail')
+        }
     }
 }
 
-const car = new Car({
-    status:'New',
-    wheels:4,
-    avail:()=>{console.log('avail')}
-})
+class Ford extends Car {
+    constructor(){
+    super()
+    }
+}
 
-console.log(car)
+const car = new Car()
+const ford = new Ford()
+
+
+ford.color = 'red';
+console.log(ford)
+
+// console.log(car)
