25b2923faef567ce3421593694b8902750917278
Asad Ikhlas <asadikhlas03@gmail.com>
2019-03-26 22:46:02 +0500

    commit

---
+++ classes/classes_1.js
@@ -0,0 +1,11 @@
+function Car(){}
+var car = new Car()
+
+Car.prototype.status = 'New';
+Car.prototype.wheels = 4;
+Car.prototype.avail = function(){
+    console.log('available')
+}
+var ford = new Car()
+
+console.log(ford.__proto__)
