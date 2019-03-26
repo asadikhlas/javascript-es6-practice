1ada12514ff8df1fa54a0d7ae93da53d0b619494
Asad Ikhlas <asadikhlas03@gmail.com>
2019-03-26 22:18:36 +0500

    commit

---
+++ Rest operator/rest_operator_1.js
@@ -0,0 +1,19 @@
+ //ES5 CODING
+
+//  function args(arg1,arg2,arg3){
+//          const arguments = [arg1,arg2,arg3]
+//     console.log(arguments)
+//     }
+
+// args("val 1",'val 2','val 3')
+
+
+//ES6 CODING
+
+
+function args(...args){
+    console.log(`My name is ${args[0]} and i am ${args[1]} and my eyes is ${args[2]}`)
+}
+
+args('asad','22','brown')
+
