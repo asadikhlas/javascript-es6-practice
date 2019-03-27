26e2c5d00529e998b02b3fe7cc2f12ceabe48dcf
Asad Ikhlas <asadikhlas03@gmail.com>
2019-03-27 22:38:36 +0500

    comit

---
+++ Destructuring/destructuring_4.js
@@ -0,0 +1,13 @@
+ function createCar ({type,brand,model,color,year}){
+
+ }
+
+ const car = {
+     brand:'ford',
+     model:'focus',
+     color:'red',
+     year:'2015',
+     type:'fast'
+ }
+
+ createCar(car)
