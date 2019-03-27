bcecc67bf97907b9b81817401fac6114355940ce
Asad Ikhlas <asadikhlas03@gmail.com>
2019-03-27 22:23:34 +0500

    commit

--- Destructuring/destructuring_2.js
+++ Destructuring/destructuring_2.js
@@ -1,11 +1,9 @@
-const cars = ['camera','phone','A5']
-
+const cars = ["camera", "phone", "A5"];
 
 // const [camera,phone,A5] = cars
 
 // console.log(phone)
 
+const [camera, ...rest] = cars;
 
-const [camera,...rest] = cars
-
-console.log(rest)
+console.log(rest);
