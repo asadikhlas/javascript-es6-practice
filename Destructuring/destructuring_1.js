7e79a9abfef331f1ae9577e7fd8ef6eb4a8bb0f8
Asad Ikhlas <asadikhlas03@gmail.com>
2019-03-27 22:03:19 +0500

    comit

---
+++ Destructuring/destructuring_1.js
@@ -0,0 +1,15 @@
+//ES5 CODE
+
+var user = {
+    name:'asad',
+    lastname:'ikhlas',
+    age:22
+}
+
+var name = user.name;
+var lastname = user.lastname;
+var age = user.age;
+
+console.log(name)
+console.log(lastname)
+console.log(age)
