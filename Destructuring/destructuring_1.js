1595b19aeb16d3d6131f6131775c2b0c02836565
Asad Ikhlas <asadikhlas03@gmail.com>
2019-03-27 22:07:51 +0500

    commit

--- Destructuring/destructuring_1.js
+++ Destructuring/destructuring_1.js
@@ -1,14 +1,31 @@
 //ES5 CODE
 
-var user = {
+// var user = {
+//     name:'asad',
+//     lastname:'ikhlas',
+//     age:22
+// }
+
+// var name = user.name;
+// var lastname = user.lastname;
+// var age = user.age;
+
+// console.log(name)
+// console.log(lastname)
+// console.log(age)
+
+
+//ES6 CODE
+
+const user = {
     name:'asad',
     lastname:'ikhlas',
     age:22
 }
 
-var name = user.name;
-var lastname = user.lastname;
-var age = user.age;
+const {name} = user;
+const {lastname} = user;
+const {age} = user;
 
 console.log(name)
 console.log(lastname)
