2793991bf189c62c216d95ef168ec34c9d4b4ce8
Asad Ikhlas <asadikhlas03@gmail.com>
2019-03-28 22:41:28 +0500

    commit

--- Promises and fetch/promises_and_fetch_1.js
+++ Promises and fetch/promises_and_fetch_1.js
@@ -2,29 +2,26 @@
 //Resolved , Fulfiled
 //Rejected
 
+let promise = new Promise((resolved, rejected) => {
+  setTimeout(() => {
+    // resolved()
+    rejected();
+  }, 5000);
+});
 
-let promise = new Promise((resolved, rejected)=>{
-    setTimeout(()=>{
-        // resolved()
-        rejected()
-    },5000)
-})
+promise.then(() => console.log("finished")).catch(() => console.log("damn"));
 
-promise.then(()=>console.log('finished'))
-.catch(()=>console.log('damn'))
+// const url = 'https://jsonplaceholder.typeicode.com/posts/1';
+// fetch(url,{
+//     method:'POST',
+//     headers: {
+//         'content-type':'application/json'
+//     },
+//     body:JSON.stringify({title:'some dum title'}),
+//     mode: 'cors',
+//     redirect:xxx,
+// })
 
-
-const url = 'https://jsonplaceholder.typeicode.com/posts/1';
-fetch(url,{
-    method:'POST',
-    headers: {
-        'content-type':'application/json'
-    },
-    body:JSON.stringify({title:'some dum title'}),
-    mode: 'cors',
-    redirect:xxx,
-})
-
-.then(response => response.json())
-.then(data => console.log(data))
-.catch(error => console.log('error show', error))
+// .then(response => response.json())
+// .then(data => console.log(data))
+// .catch(error => console.log('error show', error))
