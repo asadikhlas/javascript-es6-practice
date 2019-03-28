0bd35f07867cc711fbba6523280a2aa06c7f1502
Asad Ikhlas <asadikhlas03@gmail.com>
2019-03-28 22:53:06 +0500

    commit

--- String_and_numbers/string_and_numbers.js
+++ String_and_numbers/string_and_numbers.js
@@ -6,5 +6,11 @@
 // var word = 'camera';
 // console.log(word.startsWith('am',1))
 
-var word = 'camera';
-console.log(word.endsWith('era'))
+// var word = 'camera';
+// console.log(word.endsWith('era'))
+
+console.log(Number.isSafeInteger(200))
+console.log(Number.isSafeInteger(0.5411512441))
+
+
+
