4d468c7c512eff38f85dcf3dae3b45b25f96a5b5
Asad Ikhlas <asadikhlas03@gmail.com>
2019-03-28 22:41:12 +0500

    commit

--- Promises and fetch/promises_and_fetch_1.js
+++ Promises and fetch/promises_and_fetch_1.js
@@ -12,3 +12,19 @@ let promise = new Promise((resolved, rejected)=>{
 
 promise.then(()=>console.log('finished'))
 .catch(()=>console.log('damn'))
+
+
+const url = 'https://jsonplaceholder.typeicode.com/posts/1';
+fetch(url,{
+    method:'POST',
+    headers: {
+        'content-type':'application/json'
+    },
+    body:JSON.stringify({title:'some dum title'}),
+    mode: 'cors',
+    redirect:xxx,
+})
+
+.then(response => response.json())
+.then(data => console.log(data))
+.catch(error => console.log('error show', error))
