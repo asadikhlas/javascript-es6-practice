bee8e81d43c38ab8fa460d70ef2240293bed0777
Asad Ikhlas <asadikhlas03@gmail.com>
2019-03-29 22:18:26 +0500

    comit

---
+++ Generators/generator_2.js
@@ -0,0 +1,16 @@
+function *brands(){
+    yield 'Ford';
+    yield 'Nissan';
+    yield 'Chevy';
+    yield 'Toyota';
+    yield 'Honda';
+}
+const gen = brands()
+
+console.log(gen.next())
+console.log(gen.next())
+console.log(gen.next())
+console.log(gen.next())
+console.log(gen.next())
+console.log(gen.next())
+console.log(gen.next())
