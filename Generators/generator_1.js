234b209050f52973a0f85b971b52c7a246d6e2f6
Asad Ikhlas <asadikhlas03@gmail.com>
2019-03-29 22:14:56 +0500

    commit

---
+++ Generators/generator_1.js
@@ -0,0 +1,23 @@
+
+
+function getOrder(){
+    return 'Pizza and cake'
+}
+
+function* lotGenerator (){
+
+
+    alert('HELLO, how can i help you')
+    yield 'step 1 completed'
+
+    alert(`your order was ${getOrder()}`)
+    yield 'step 2 completed'
+
+    alert('thanks for eating here')
+    yield 'step 3 completed'
+
+    return 'finished'
+
+}
+
+let gen = lotGenerator();
