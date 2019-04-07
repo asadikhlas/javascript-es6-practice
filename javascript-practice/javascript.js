f99ccce30c9b484769c1f6860e0fd495bccbccfb
Asad Ikhlas <asadikhlas03@gmail.com>
2019-04-07 19:42:27 +0500

    commit

--- javascript-practice/javascript.js
+++ javascript-practice/javascript.js
@@ -1,6 +1,10 @@
-const numbers = [1, 2, 3, 4, 5];
-const otherArray = numbers.map(function(number){
-    return number * 2
-})
+// const numbers = [1, 2, 3, 4, 5];
+// const otherArray = numbers.map(function(number){
+//     return number * 2
+// })
 
-console.log(otherArray)
+// console.log(otherArray)
+
+const number = 80.6
+let newNumber = Math.round(number)
+console.log(newNumber)
