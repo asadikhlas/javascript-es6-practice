fc90be4a6148b193eb71cf15011d6c0e6f21a41f
Asad Ikhlas <asadikhlas03@gmail.com>
2019-04-07 19:47:38 +0500

    commit

--- javascript-practice/javascript.js
+++ javascript-practice/javascript.js
@@ -5,6 +5,18 @@
 
 // console.log(otherArray)
 
-const number = 80.6
-let newNumber = Math.round(number)
-console.log(newNumber)
+// const number = 80.6
+// let newNumber = Math.round(number)
+// console.log(newNumber)
+
+const returnemploye = () =>{
+    const name = "Asad";
+    const age = 22;
+    const position = "manager";
+
+    console.log(`My name is ${name} my age is ${age} my position is ${position}`)
+}
+
+returnemploye();
+
+
